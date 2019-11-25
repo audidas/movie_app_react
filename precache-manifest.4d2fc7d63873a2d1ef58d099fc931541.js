128a3d0a2a1c808eea03b8485fee67bc99d4b6a9
unknown <audidas@gmail.com>
2019-11-25 16:11:21 +0900

    Updates

---
+++ precache-manifest.4d2fc7d63873a2d1ef58d099fc931541.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d29a3d9bf6c02a734007c4a78f9e97fb",
+    "url": "/movie_app_react/index.html"
+  },
+  {
+    "revision": "b4113bcf9f63878ebd77",
+    "url": "/movie_app_react/static/css/main.ae4cfe19.chunk.css"
+  },
+  {
+    "revision": "6d633c2aa687e59e44fc",
+    "url": "/movie_app_react/static/js/2.4e6fdd97.chunk.js"
+  },
+  {
+    "revision": "b4113bcf9f63878ebd77",
+    "url": "/movie_app_react/static/js/main.ebd89189.chunk.js"
+  },
+  {
+    "revision": "1451023d577c98b24595",
+    "url": "/movie_app_react/static/js/runtime-main.71503ecc.js"
+  }
+]);
